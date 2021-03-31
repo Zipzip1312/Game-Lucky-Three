4abbff1aa1426a4d0aa567084e35f674d2d742e9
Andrey Shklyaruk <13mailg@gmail.com>
2021-03-31 17:41:36 +0300

    responsive typography

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg'
-import 'styles/app.scss'
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <p>lorem ipsum dolor sit amet, consectet lorem ipsum dolor sit amet</p>
     </div>
   )
 }
