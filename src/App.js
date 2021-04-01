a9713c95536f99846b1e0708c29cadcdf0710841
Andrey Shklyaruk <13mailg@gmail.com>
2021-04-01 11:04:11 +0300

    fonts and style helpers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 function App() {
   return (
     <div className="app">
-      <p>lorem ipsum dolor sit amet, consectet lorem ipsum dolor sit amet</p>
+      <h1>Грай та вигравай!</h1>
+      <h3>Призовий фонд: 20 млн грн</h3>
+      <h2>Вперед за перемогою!</h2>
+      <em>правила</em>
+      <p class="text-center">
+        хочемо знати трішки
+        <br />
+        більше про тебе ;)
+      </p>
+      <h2 class="orange">Стать?</h2>
+      <h2 class="orange">Вік?</h2>
+      <h2>+5</h2>
     </div>
   )
 }
