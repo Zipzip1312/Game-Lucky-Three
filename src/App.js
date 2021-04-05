65c66e6fe39bda7f9211b5dd34ea4bc28b7d9eab
Andrey Shklyaruk <13mailg@gmail.com>
2021-04-05 13:41:08 +0300

    routing transitions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { useRef } from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { nextScreen, prevScreen } from 'redux/reducer'
+import { CSSTransition } from 'react-transition-group'
 import WelcomeScreen from 'components/screens/WelcomeScreen'
 import RulesScreen from 'components/screens/RulesScreen'
 import FormScreen from 'components/screens/FormScreen'
@@ -6,33 +10,49 @@ import InviteScreen from 'components/screens/InviteScreen'
 import GameScreen from 'components/screens/GameScreen'
 import ScoreScreen from 'components/screens/ScoreScreen'
 
-import { useDispatch } from 'react-redux'
-import { nextScreen, prevScreen } from 'redux/reducer'
+const routes = [
+  { path: ['/', '/welcome'], name: 'Welcome', Component: WelcomeScreen },
+  { path: '/rules', name: 'Rules', Component: RulesScreen },
+  { path: '/form', name: 'Form', Component: FormScreen },
+  { path: '/invite', name: 'Invite', Component: InviteScreen },
+  { path: '/game', name: 'Game', Component: GameScreen },
+  { path: '/score', name: 'Score', Component: ScoreScreen }
+]
 
 function App() {
+  const refs = {
+    Welcome: useRef(null),
+    Rules: useRef(null),
+    Form: useRef(null),
+    Invite: useRef(null),
+    Game: useRef(null),
+    Score: useRef(null)
+  }
   const dispatch = useDispatch()
   return (
     <div className="app">
-      <div
-        style={{
-          display: 'flex',
-          marginBottom: '1rem',
-          width: 250,
-          justifyContent: 'space-around'
-        }}
-      >
+      <div className="control-buttons">
         <button onClick={() => dispatch(prevScreen())}>Go BACK</button>
         <button onClick={() => dispatch(nextScreen())}>Go NEXT</button>
       </div>
-      <Switch>
-        <Route exact path="/" component={WelcomeScreen} />
-        <Route path="/rules" component={RulesScreen} />
-        <Route path="/form" component={FormScreen} />
-        <Route path="/invite" component={InviteScreen} />
-        <Route path="/game" component={GameScreen} />
-        <Route path="/score" component={ScoreScreen} />
-        <Redirect to="/" />
-      </Switch>
+      {routes.map(({ path, name, Component }) => (
+        <Route key={path} exact path={path}>
+          {({ match }) => (
+            <CSSTransition
+              nodeRef={refs[name]}
+              in={match != null}
+              timeout={300}
+              classNames="screen"
+              unmountOnExit
+            >
+              <div className="screen" ref={refs[name]}>
+                <Component />
+              </div>
+            </CSSTransition>
+          )}
+        </Route>
+      ))}
+      <Redirect to="/" />
     </div>
   )
 }
