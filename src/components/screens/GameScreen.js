19f6ed6e7899b1a21fa53a226bfe4a657c1fb261
Andrey Shklyaruk <13mailg@gmail.com>
2021-04-10 22:50:56 +0300

    flip init

--- src/components/screens/GameScreen.js
+++ src/components/screens/GameScreen.js
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+// https://vestride.github.io/Shuffle/shuffle-with-react
+// https://seanlee.netlify.app/blogs/isotope-react
+// + https://codepen.io/ilovepku/pen/zYYKaYy
+
+// https://awesomeopensource.com/project/aholachek/react-flip-toolkit
+
+import { useState, useRef } from 'react'
 import { Redirect } from 'react-router-dom'
+import { Flipper, Flipped } from 'react-flip-toolkit'
+import shuffle from 'lodash.shuffle'
 
 export default function GameScreen() {
+  const lobby = useRef(null)
   const [played, setPlayed] = useState(false)
   const [disabled, setDisabled] = useState(false)
-  if (played) return <Redirect to="/score" />
   // ------------------------------
-  const scores = [
+  const [scores, setScores] = useState([
     1000,
     500,
     250,
@@ -23,8 +31,9 @@ export default function GameScreen() {
     0,
     0,
     0
-  ]
+  ])
   // ------------------------------
+  if (played) return <Redirect to="/score" />
 
   return (
     <div className="game">
@@ -32,13 +41,19 @@ export default function GameScreen() {
         className="game-start-btn"
         type="button"
         disabled={disabled}
-        onClick={() => setDisabled(true)}
+        onClick={() => setScores(shuffle(scores))}
       ></button>
       {/* <button onClick={() => setPlayed(true)}>PLAY</button> */}
-      <div className="game-lobby text-center">
-        {scores.map((score) => {
-          return <div className="score">{score}</div>
-        })}
+      <div className="" ref={lobby}>
+        <Flipper flipKey={scores.join('')} className="game-lobby text-center">
+          {scores.map((score, i) => {
+            return (
+              <Flipped key={i} flipId={i}>
+                <div className="score">{score}</div>
+              </Flipped>
+            )
+          })}
+        </Flipper>
       </div>
     </div>
   )
