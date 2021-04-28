1872972a51d20b4c9b0c5ab7adecc56fd00b586e
Andrey Shklyaruk <13mailg@gmail.com>
2021-04-28 15:53:34 +0300

    refactor DrawPrizes classes

--- src/components/DrawPrizes.js
+++ src/components/DrawPrizes.js
@@ -1,13 +1,14 @@
 export default function DrawPrizes({ hide = false }) {
-  const classes = 'draw-prizes flex-center white bold'
+  const wrapperClasses = 'draw-prizes flex-center white bold'
+  const prizeClasses = 'prize flex-center'
   const prizeAnimation = 'animate bounceInRight'
   const hideAnimation = 'animate hidden'
 
   return (
-    <div className={`${classes} ${hide ? hideAnimation : ''}`}>
-      <div className={`prize flex-center ${prizeAnimation} delay-2`}>1000</div>
-      <div className={`prize flex-center ${prizeAnimation} delay-1-5`}>500</div>
-      <div className={`prize flex-center ${prizeAnimation} delay-1`}>250</div>
+    <div className={`${wrapperClasses} ${hide ? hideAnimation : ''}`}>
+      <div className={`${prizeClasses} ${prizeAnimation} delay-2`}>1000</div>
+      <div className={`${prizeClasses} ${prizeAnimation} delay-1-5`}>500</div>
+      <div className={`${prizeClasses} ${prizeAnimation} delay-1`}>250</div>
     </div>
   )
 }
