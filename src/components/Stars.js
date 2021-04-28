2769c6ad1ad808ed63876786c1dc5dbf72784f81
Andrey Shklyaruk <13mailg@gmail.com>
2021-04-28 15:44:42 +0300

    upd active stars animation

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -4,13 +4,14 @@ export default function Stars() {
   const rulesAccepted = useSelector((state) => state.app.rulesAccepted)
   const formFilled = useSelector((state) => state.app.formFilled)
   const doneInviting = useSelector((state) => state.app.doneInviting)
+  const activeClasses = 'active animate tada'
 
   return (
     <div className="stars">
       <div className="flex-center">
-        <div className={`star ${rulesAccepted ? 'active' : ''}`}></div>
-        <div className={`star ${formFilled ? 'active' : ''}`}></div>
-        <div className={`star ${doneInviting ? 'active' : ''}`}></div>
+        <div className={`star ${rulesAccepted ? activeClasses : ''}`}></div>
+        <div className={`star ${formFilled ? activeClasses : ''}`}></div>
+        <div className={`star ${doneInviting ? activeClasses : ''}`}></div>
       </div>
     </div>
   )
