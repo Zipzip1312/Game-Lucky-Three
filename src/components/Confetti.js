caa00dc8a1647685ea3b3dce82d30577f29ab007
Andrey Shklyaruk <13mailg@gmail.com>
2021-05-12 19:05:26 +0300

    fix confetti

--- src/components/Confetti.js
+++ src/components/Confetti.js
@@ -8,17 +8,17 @@ export default function Confetti() {
     const frame = () => {
       // launch a few confetti from the left edge
       confetti({
-        particleCount: 10,
+        particleCount: 5,
         angle: 60,
-        spread: 55,
+        spread: 45,
         origin: { x: 0 },
         zIndex: -1
       })
       // and launch a few from the right edge
       confetti({
-        particleCount: 10,
+        particleCount: 5,
         angle: 120,
-        spread: 55,
+        spread: 45,
         origin: { x: 1 },
         zIndex: -1
       })
